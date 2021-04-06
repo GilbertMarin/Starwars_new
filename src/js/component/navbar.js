f2e79e177eb4808857a4ed391cf953ac83124816
Joshua <marichaljoshua@gmail.com>
2021-04-06 02:47:58 +0000

    Star Wars Final

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Favorites } from "./favorites";
+import { Favorites } from "./favorite";
 
 export const Navbar = () => {
 	return (
